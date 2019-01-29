84ffeb99c65cda819a97b1636ffcda815f07a8b6
wmorrow4 <willardsmorrowiv@gmail.com>
2019-01-29 12:19:15 -0500

    added contacts controller and dummy functions

--- src/server/controllers/login.ts
+++ src/server/controllers/login.ts
@@ -12,3 +12,14 @@ module.exports.signup = function(req:any, res:any, next:any) {
     res.send(JSON.stringify({message: "It worked!"}, null, 2))
     res.end()
 };
+
+module.exports.userLogin = function(req:any, res:any, next:any) {
+
+    // print out the params
+    console.log(util.inspect(req.swagger.params, false, Infinity, true))
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200)
+    res.send(JSON.stringify({message: "It worked!"}, null, 2))
+    res.end()
+};
