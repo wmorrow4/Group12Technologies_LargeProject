d126a0a19204a43e01391de1aa6bf005d1d66687
Jarrod Pearman <pearmanjj@gmail.com>
2019-02-05 18:36:13 -0500

    Jarrod2

--- src/server/controllers/contacts.ts
+++ src/server/controllers/contacts.ts
@@ -67,30 +67,34 @@ module.exports.deleteContact = function(req:any, res:any, next:any) {
     
     // print out the params 
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
-
-    if(req.swagger.params.userinfo.value.FirstName) {
-
-        var myobj = req.swagger.params.userinfo.value;
-        myobj.UserID = req.session.username;
-
-        db.contacts.insertOne( req.swagger.params.userinfo.value , function(err:any, res:any) {
-            if (err) throw err;
-            console.log("1 document inserted");                       
-      });   
-        res.status(OK)
-        res.send(JSON.stringify({ message: "It worked!" }, null, 2))
-        res.end()     
-    }
-    else {
-        res.status(BadRequest)
-        res.send(JSON.stringify({ message: "At least a first name is required" }, null, 2))
-        res.end()
-    }
-
     res.setHeader('Content-Type', 'application/json')
-    res.status(200)
-    res.send(JSON.stringify({message: "Contact Deleted"}, null, 2))
-    res.end()
+
+    var myobj = req.swagger.params.userinfo.value;
+    myobj.UserID = req.session.username;
+
+    var temp = true;
+    db.contacts.findOne(req.swagger.params.userinfo.value, function(err:any,result) {
+        if (err){}            
+        if(!result){
+            res.status(BadRequest)
+            res.send(JSON.stringify({ message: "Contact Doesnt Exist" }, null, 2))
+            res.end()
+        }
+        else{
+            db.contacts.deleteOne( myobj , function(err:any, res:any) {
+                if (err) throw err;
+                console.log("1 document deleted");                       
+            });   
+            res.status(OK)
+            res.send(JSON.stringify({ message: "It worked!" }, null, 2))
+            res.end()
+        }
+            
+    });
+    
+
+   
+    
 };
 
 
