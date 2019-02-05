30f7d0df6336729af81ded9e901c2a41ae752e08
Jarrod Pearman <pearmanjj@gmail.com>
2019-02-05 16:07:32 -0500

    Create contact updated

--- src/server/controllers/contacts.ts
+++ src/server/controllers/contacts.ts
@@ -1,30 +1,44 @@
 'use strict';
 
 import util = require('util')
+import session = require('express-session')
+import swaggerTools = require('swagger-tools')
 import db = require('../db')
 
-var url = "mongodb://localhost:27017/";
+const OK = 200
+const BadRequest = 400
+const InternalServerError = 500
+
+const inspect = (input: any) => util.inspect(input, false, Infinity, true)
+
 
 module.exports.createContact = function(req:any, res:any, next:any) {
 
     // print out the params
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
-
-    var MongoClient = require('mongodb').MongoClient;    
-
-    MongoClient.connect(url, function(err:any, db:any) {
-    if (err) throw err;
-    var dbo = db.db("mean");    
-    dbo.collection("contacts").insert( req.swagger.params.userinfo.value , function(err:any, res:any) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-  });
-})
     res.setHeader('Content-Type', 'application/json')
-    res.status(200)
-    res.send(JSON.stringify({message: "Contact Created"}, null, 2))
-    res.end()
+    
+        
+    if(req.swagger.params.userinfo.value.FirstName) {
+
+        var myobj = req.swagger.params.userinfo.value;
+        myobj.UserID = req.session.username;
+
+        db.contacts.insertOne( req.swagger.params.userinfo.value , function(err:any, res:any) {
+            if (err) throw err;
+            console.log("1 document inserted");                       
+      });   
+        res.status(OK)
+        res.send(JSON.stringify({ message: "It worked!" }, null, 2))
+        res.end()     
+    }
+    else {
+        res.status(BadRequest)
+        res.send(JSON.stringify({ message: "At least a first name is required" }, null, 2))
+        res.end()
+    }
+    
+    
 };
 
 module.exports.listContacts = function(req:any, res:any, next:any) {
@@ -50,21 +64,28 @@ module.exports.updateContact = function(req:any, res:any, next:any) {
 };
 
 module.exports.deleteContact = function(req:any, res:any, next:any) {
-
-    // print out the params
+    
+    // print out the params 
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
 
-    var MongoClient = require('mongodb').MongoClient;    
+    if(req.swagger.params.userinfo.value.FirstName) {
 
-    MongoClient.connect(url, function(err:any, db:any) {
-    if (err) throw err;
-    var dbo = db.db("mean");    
-    dbo.collection("contacts").deleteOne( req.swagger.params.userinfo.value , function(err:any, res:any) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        db.close();
-  });
-})
+        var myobj = req.swagger.params.userinfo.value;
+        myobj.UserID = req.session.username;
+
+        db.contacts.insertOne( req.swagger.params.userinfo.value , function(err:any, res:any) {
+            if (err) throw err;
+            console.log("1 document inserted");                       
+      });   
+        res.status(OK)
+        res.send(JSON.stringify({ message: "It worked!" }, null, 2))
+        res.end()     
+    }
+    else {
+        res.status(BadRequest)
+        res.send(JSON.stringify({ message: "At least a first name is required" }, null, 2))
+        res.end()
+    }
 
     res.setHeader('Content-Type', 'application/json')
     res.status(200)
