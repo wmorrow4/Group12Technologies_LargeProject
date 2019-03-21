ba91b25fa7749728bcb8143d8b2e7127daf6897b
kbeason42 <42628624+kbeason42@users.noreply.github.com>
2019-03-20 22:23:57 -0400

    Update schedule.ts
    Made some super basic skeletons for the different functions

--- src/server/controllers/schedule.ts
+++ src/server/controllers/schedule.ts
@@ -1,1 +1,48 @@
+'use strict';
 
+import util = require('util')
+import swaggerTools = require('swagger-tools')
+import db = require('../db')
+import api = require('../api')
+import UserInfo = db.UserInfo
+
+const OK = 200
+const BadRequest = 400
+const InternalServerError = 500
+
+const inspect = (input: any) => util.inspect(input, false, Infinity, true)
+
+module.exports.ClaimAppointment = function (req: any, res: any, next: any) {
+    // print out the params
+    console.log(inspect(req.swagger.params))
+    res.setHeader('Content-Type', 'application/json')
+    res.end()
+} 
+
+module.exports.ListSchedules = function (req: any, res: any, next: any) {
+    // print out the params
+    console.log(inspect(req.swagger.params))
+    res.setHeader('Content-Type', 'application/json')
+    res.end()
+} 
+
+module.exports.SchedulerEditInfo = function (req: any, res: any, next: any) {
+    // print out the params
+    console.log(inspect(req.swagger.params))
+    res.setHeader('Content-Type', 'application/json')
+    res.end()
+} 
+
+module.exports.UserEditInfo = function (req: any, res: any, next: any) {
+    // print out the params
+    console.log(inspect(req.swagger.params))
+    res.setHeader('Content-Type', 'application/json')
+    res.end()
+} 
+
+module.exports.ListAppointments = function (req: any, res: any, next: any) {
+    // print out the params
+    console.log(inspect(req.swagger.params))
+    res.setHeader('Content-Type', 'application/json')
+    res.end()
+} 
