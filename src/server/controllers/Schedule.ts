8384078392551940fabd42c06a5c3cd7a6e6ebdf
blaiadam <blaiadam7777@gmail.com>
2019-03-23 21:12:59 -0400

    Updated Schedule.ts

---
+++ src/server/controllers/Schedule.ts
@@ -0,0 +1,39 @@
+module.exports.createSchedule = function (req: api.Request & swaggerTools.Swagger20Request<CreateSchedulePayload>, res: express.Response) {
+	
+	console.log(util.inspect(req.swagger.params, false, Infinity, true))
+	res.setHeader('Content-Type', 'application/json')
+	
+	if(req.swagger.params.schedule.value.name && req.swagger.params.schedule.value.open
+	   && req.swagger.params.schedule.value.close && req.swagger.params.schedule.value.length
+	   && req.swagger.params.schedule.value.capacity && req.session) {
+		
+		var myobj = req.swagger.params.schedule.value;
+		
+		myobj.belongsTo = new ObjectID(req.session.userid);
+		
+		db.schedules.insertOne(myobj, function (err: MongoError, result: InsertOneWriteOpResult) {
+            if (err) {
+                res.status(InternalServerError)
+                res.send(JSON.stringify({ message: inspect(err) }, null, 2))
+                res.end()
+            }
+            if (!result) {
+                res.status(BadRequest)
+                res.send(JSON.stringify({ message: "Create Schedule Failed" }, null, 2))
+                res.end()
+            }
+            else {
+                res.status(OK)
+                res.send(JSON.stringify({ message: "Schedule Inserted Successfully " }, null, 2))
+                res.end()
+                console.log("1 document inserted");
+            }
+        });
+
+    }
+    else {
+        res.status(BadRequest)
+        res.send(JSON.stringify({ message: "All fields must be filled" }, null, 2))
+        res.end()
+    }
+};
