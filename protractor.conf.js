f320e2a43c53f940513c85a3c197dc35c8fe7b27
blaiadam <blaiadam7777@gmail.com>
2019-04-19 12:30:27 -0400

    Took source code from mean-contact-list.

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,7 +6,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
@@ -19,10 +19,12 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare() {
+  beforeLaunch: function() {
     require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
+      project: 'e2e/tsconfig.e2e.json'
     });
+  },
+  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
