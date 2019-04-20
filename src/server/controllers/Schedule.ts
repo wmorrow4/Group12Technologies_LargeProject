2b13fb015aeb68bcbb8b420c4c5761d5be5c4291
U-DESKTOP-2BMLU6A\Billy <Willard Morrow>
2019-04-20 15:41:26 -0400

    made changes to swagger and db.ts for day start end

--- src/server/controllers/Schedule.ts
+++ src/server/controllers/Schedule.ts
@@ -58,7 +58,7 @@ module.exports.CreateSchedule = function (req: api.Request & swaggerTools.Swagge
             res.send(JSON.stringify({ message: "Login required" }, null, 2))
             res.end()
         }
-        if (req.swagger.params.schedule.value.schedule_name && req.swagger.params.schedule.value.average_appointment_length && req.swagger.params.schedule.value.max_capacity && req.swagger.params.schedule.value.M && req.swagger.params.schedule.value.T && req.swagger.params.schedule.value.W && req.swagger.params.schedule.value.Th && req.swagger.params.schedule.value.F && req.swagger.params.schedule.value.S && req.swagger.params.schedule.value.Su) {
+        if (req.swagger.params.schedule.value.schedule_name && req.swagger.params.schedule.value.average_appointment_length && req.swagger.params.schedule.value.max_capacity && req.swagger.params.schedule.value.M_START && req.swagger.params.schedule.value.M_END && req.swagger.params.schedule.value.T_START && req.swagger.params.schedule.value.T_END && req.swagger.params.schedule.value.W_START && req.swagger.params.schedule.value.W_END && req.swagger.params.schedule.value.Th_START && req.swagger.params.schedule.value.Th_END && req.swagger.params.schedule.value.F_START && req.swagger.params.schedule.value.F_END && req.swagger.params.schedule.value.S_START && req.swagger.params.schedule.value.S_END && req.swagger.params.schedule.value.Su_START && req.swagger.params.schedule.value.Su_END) {
 
             var scheduleObject = req.swagger.params.schedule.value;
             scheduleObject.schedulerID = new ObjectID(req.session.logid);
